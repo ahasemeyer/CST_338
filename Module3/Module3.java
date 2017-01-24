e96b60be9bed0fe958aae09fdf751771f44a2a7e
Austin Hasemeyer <ahasemeyer@csumb.edu>
2017-01-23 18:56:35 -0800

    adding Module3

---
+++ Module3/Module3.java
@@ -0,0 +1,18 @@
+/**
+ * @author Austin Hasemeyer
+ * @author Samuel Roy
+ * @author Ali Asrani
+
+ * Programming Assignment 3
+ * School: CSU, Monterey Bay
+ * Course: CST 338 Software Design
+ * Professor: Jesse Cecil, MS
+ */
+ 
+ public class Module3
+ {
+    public static void main(String[] arg)
+    {
+       
+    }
+ }
