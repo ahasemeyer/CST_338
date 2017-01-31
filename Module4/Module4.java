e5b6eb6b323868c3b68a3b8b63974828f63cb34f
Austin Hasemeyer <ahasemeyer@csumb.edu>
2017-01-30 20:22:17 -0800

    add

--- Module4/Module4.java
+++ Module4/Module4.java
@@ -12,7 +12,10 @@ Professor: Jesse Cecil, MS
 
 public class Module4
 {
-   
+   public static main(String[] args)
+   {
+      BarcodeImage.Test();
+   }
 }
 
 public interface BarcodeIO
@@ -28,8 +31,39 @@ public interface BarcodeIO
 public class BarcodeImage implements Cloneable
 {
    public static final int MAX_HEIGHT = 30;
+   public static final int MAX_WIDTH = 65;
    
    private boolean[][] image_data;
+   
+   public BarcodeImage()
+   {
+      image_data[][] = new char[MAX_WIDTH][MAX_HEIGHT];
+      for(int i = 0; i < MAX_WIDTH; i++)
+      {
+         for(int j = 0; j < MAX_HEIGHT; j++)
+         {
+            image_data[i][j] = '*';
+         }
+      } 
+   }
+   
+   public displayToConsole()
+   {
+      for(int i = 0; i < MAX_WIDTH; i++)
+      {
+         for(int j = 0; j < MAX_HEIGHT; j++)
+         {
+            System.out.print(image_data[i][j]);
+         }
+         System.out.println();
+      }
+   }
+   
+   
+   public void Test()
+   {
+      
+   }
    /*
     public Object clone()
     {
